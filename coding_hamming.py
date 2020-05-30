22c8240191aee594176bbd4f77267d15db134f3a
jcorreagallar <jcorreagallar@gmail.com>
2020-05-29 21:15:06 -0400

    detect error by hamming 29/05/2020

--- coding_hamming.py
+++ coding_hamming.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import randint
 
 def redundancyBits(l): 
     for k in range(l): 
@@ -13,10 +14,10 @@ def generateMatrix(l,c):
     gen_matrix = np.append(gen_matrix,new_row, axis=0)
   return gen_matrix
 
-def operationMatrix(work,gen_matrix):
+def operationMatrix(word,gen_matrix):
     N = len(gen_matrix)
-    l = len(work)
-    result = work * gen_matrix
+    l = len(word)
+    result = word * gen_matrix
     aux_code = np.zeros((1,l),dtype = int)
     aux_bits = np.zeros((1,N-l),dtype = int)
 
@@ -51,10 +52,48 @@ def orderedCode(code,bits_parity):
         code = np.reshape(code,(1,len(code)))
     return hamming
 
-work = [0,1,1,0]
+def tx(message):
+    pos = randint(0,len(message[0])-1)
+    message[0][pos] = randint(0,1)
+    return message 
+
+def rx(receive,c):
+  l = len(receive[0]) - c
+  redundant_row = []
+  data_row = []
+
+  for pos in range(1,len(receive[0]) + 1):
+    if len(redundant_row) < c and (pos == 1 or pos % 2 == 0):
+      redundant_row = np.append(redundant_row, receive[0][pos - 1])
+    else:
+      data_row = np.append(data_row, receive[0][pos - 1])
+  
+  matrix = generateMatrix(l,c)
+  matrix = matrix[l:]
+  result = np.array(data_row, dtype=int) * matrix
+  aux_bits = np.zeros((1,c),dtype = int)
+  for i in range(len(matrix)):
+      vec_aux = result[i]
+      vec_aux = np.delete(vec_aux,i)
+      P = None
+      for j in range(len(vec_aux)):
+        if j > 1:
+          P = P ^ vec_aux[j]
+        elif j == 1:
+          P = vec_aux[j] ^ vec_aux[j-1]
+      aux_bits[0][i] = P ^ np.array(redundant_row[i],dtype=int)
+  return aux_bits.dot(2**np.arange(aux_bits.size)[::-1])
+
+word = [0,1,1,0]
 l = len(work)
 c = redundancyBits(l)
 gen_matrix = generateMatrix(l,c)
 code, bits_parity= operationMatrix(work,gen_matrix)
 hamming = orderedCode(code,bits_parity)
-print(hamming)
+print("Código enviado:", hamming)
+send = tx(hamming)
+print("Código recibido:", send)
+result = rx(send,c)
+print("Error en el bit:" , result)
+
+
